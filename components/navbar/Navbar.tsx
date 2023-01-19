a6c743db5ce84bf4ee7e86a6e9fb1ce4d1617b4a
brianslab <bdkuhn602@gmail.com>
2023-01-19 05:49:21 -0800

    onhover styling and remove ul

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,32 +4,28 @@ import { NavbarProps } from './types';
 export function Navbar({ home = 'home', pages = [] }: NavbarProps) {
   const pageButtons = pages.map((page) => {
     return (
-      <li className='float-right' key={page.name}>
-        <Link
-          className='block pr-2 lg:px-2 py-2 text-gray-600 hover:shadow-md hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out'
-          href={page.link}
-        >
-          {page.name}
-        </Link>
-      </li>
+      <Link
+        className='block px-2 py-2 text-gray-600 hover:shadow-md transition duration-150 ease-in-out'
+        href={page.link}
+      >
+        {page.name}
+      </Link>
     );
   });
 
   return (
     <header>
       <nav className='shadow-md py-2 bg-white w-full relative flex items-center justify-between'>
-        <div className='px-6 flex-wrap grow'>
+        <div className='px-6 flex-wrap grow block'>
           <Link
-            className='float-left block pr-2 lg:px-2 py-2 text-gray-600 hover:shadow-md hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out'
+            className='float-left px-2 py-2 text-gray-600 hover:shadow-md transition duration-150 ease-in-out'
             href='/'
             // data-mdb-ripple='true'
             // data-mdb-ripple-color='dark'
           >
             {home}
           </Link>
-          <div className='ml-auto'>
-            <ul>{pageButtons}</ul>
-          </div>
+          <div className='flex float-right'>{pageButtons}</div>
         </div>
       </nav>
     </header>
