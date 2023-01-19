e9cb8136940fd0f38a960d37ecc8fc5435d40a1e
brianslab <bdkuhn602@gmail.com>
2023-01-19 05:53:53 -0800

    finish styling

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -15,18 +15,14 @@ export function Navbar({ home = 'home', pages = [] }: NavbarProps) {
 
   return (
     <header>
-      <nav className='shadow-md py-2 bg-white w-full relative flex items-center justify-between'>
-        <div className='px-6 flex-wrap grow block'>
-          <Link
-            className='float-left px-2 py-2 text-gray-600 hover:shadow-md transition duration-150 ease-in-out'
-            href='/'
-            // data-mdb-ripple='true'
-            // data-mdb-ripple-color='dark'
-          >
-            {home}
-          </Link>
-          <div className='flex float-right'>{pageButtons}</div>
-        </div>
+      <nav className='shadow-md px-2 py-2 flex flex-wrap grow block bg-white w-full relative items-center justify-between'>
+        <Link
+          className='float-left px-2 py-2 text-gray-600 hover:shadow-md transition duration-150 ease-in-out'
+          href='/'
+        >
+          {home}
+        </Link>
+        <div className='flex float-right'>{pageButtons}</div>
       </nav>
     </header>
   );
